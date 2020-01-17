7c64f2bb06015367f20351afd94b2bdb4f29c446
raseshsingh <raseshyunique@gmail.com>
2020-01-17 20:24:10 +0530

    init

--- IgnouHackathon2020/settings.py
+++ IgnouHackathon2020/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django-crispy-forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,8 +73,10 @@ WSGI_APPLICATION = 'IgnouHackathon2020.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+        },
     }
 }
 
